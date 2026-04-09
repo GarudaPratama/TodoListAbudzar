7d683fefe1ee5dbbe5369d2a0cb3a6bc9e7ab78c
GarudaPratama <garuda281209@student.abudzar.sch.id>
2026-04-09 13:41:03 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.jsx'
+import ListTodo from "./ListTodo.jsx"
+import AddTodo from "./AddTodo.jsx"
+import UpdateTodo from "./UpdateTodo.jsx"
+import Counter from "./Counter.jsx"
+import { counterSlice } from "./counterSlice.js"
+import { todoListSlice } from "./todoListSlice.js"
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    todoList: todoListSlice.reducer,
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/todolist"} element={<ListTodo/>}/>
+          <Route path={"/todolist/add"} element={<AddTodo/>}/>
+          <Route path={"/todolist/:id/edit"} element={<UpdateTodo/>}/>
+          <Route path={"/"} element={<App/>}/>
+          <Route path={"/counter"} element={
+            <>
+              <Counter/>
+              <Counter/>
+            </>
+          }/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
---
+++ src/useTodoList.js
@@ -0,0 +1,28 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const todoListSlice = createSlice({
+  name: "todoList",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      const {name} = action.payload;
+      state.push({name, id: id++});
+    },
+    removeTodo: (state, action) => {
+      const {id} = action.payload;
+      const index = state.findIndex(todo => todo.id === id);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
+    updateTodo(state, action) {
+      const {id, name} = action.payload;
+      const todo = state.find(todo => todo.id === id);
+      if (todo) {
+        todo.name = name;
+      }
+    }
+  }
+})
+
+export const {addTodo, removeTodo, updateTodo} = todoListSlice.actions;
