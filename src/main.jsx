6589d9e02c06d16355a19c187e506615b5edbfd9
GarudaPratama <garuda281209@student.abudzar.sch.id>
2026-04-10 11:14:37 +0700

    pp

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
-import { BrowserRouter, Routes, Route } from 'react-router'
+// Gunakan 'react-router-dom' jika 'react-router' saja tidak jalan di versimu
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
+
 import App from './App.jsx'
 import ListTodo from "./ListTodo.jsx"
 import AddTodo from "./AddTodo.jsx"
 import UpdateTodo from "./UpdateTodo.jsx"
-import Counter from "./Counter.jsx"
-import { counterSlice } from "./counterSlice.js"
+import Counter from "./Counter.jsx" // <--- HARUS DI-IMPORT
+
+// IMPORT KEDUA SLICE
+import { counterSlice } from "./counterSlice.js" // <--- PASTIKAN SUDAH ADA FILENYA
 import { todoListSlice } from "./todoListSlice.js"
 
 const store = configureStore({
   reducer: {
+    // Pastikan memanggil .reducer dari objek slice-nya
     counter: counterSlice.reducer,
     todoList: todoListSlice.reducer,
   }
@@ -23,11 +28,11 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path={"/todolist"} element={<ListTodo/>}/>
-          <Route path={"/todolist/add"} element={<AddTodo/>}/>
-          <Route path={"/todolist/:id/edit"} element={<UpdateTodo/>}/>
-          <Route path={"/"} element={<App/>}/>
-          <Route path={"/counter"} element={
+          <Route path="/todolist" element={<ListTodo/>}/>
+          <Route path="/todolist/add" element={<AddTodo/>}/>
+          <Route path="/todolist/:id/edit" element={<UpdateTodo/>}/>
+          <Route path="/" element={<App/>}/>
+          <Route path="/counter" element={
             <>
               <Counter/>
               <Counter/>
@@ -36,5 +41,5 @@ createRoot(document.getElementById('root')).render(
         </Routes>
       </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
